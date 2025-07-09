65c774580217f31462cf8c05c5f1b6ef2b5dd0e5
KYS <ehcl1027@gmail.com>
2025-07-10 01:06:29 +0900

    feat: add user profile fields and link EmergencyContact entity
    - Added birthDate, phoneNumber, address, profileImageUrl fields to User
    - Established OneToMany relation with EmergencyContact

--- CARING-Back-User/src/main/java/com/caring/user_service/domain/user/entity/User.java
+++ CARING-Back-User/src/main/java/com/caring/user_service/domain/user/entity/User.java
@@ -1,15 +1,30 @@
 package com.caring.user_service.domain.user.entity;
 
 import com.caring.user_service.domain.auditing.entity.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Getter
@@ -36,6 +51,27 @@ public class User extends BaseTimeEntity implements UserDetails {
     private String name;
     @Column(name = "shelter_uuid")
     private String shelterUuid;
+
+    @Column(name = "birth_date")
+    private String birthDate; // ex: 19980505 (String)
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "road_address") // 도로명 주소
+    private String roadAddress;
+
+    @Column(name = "detail_address") // 상세 주소
+    private String detailAddress;
+
+    @Column(name = "postal_code", length = 10) // 우편번호
+    private String postalCode;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EmergencyContact> emergencyContacts = new ArrayList<>();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
