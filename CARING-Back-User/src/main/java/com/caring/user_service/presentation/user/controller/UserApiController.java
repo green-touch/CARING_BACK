49409cf5fab145e1974b3e95a096538d357c7b4d
KYS <ehcl1027@gmail.com>
2025-07-10 11:50:43 +0900

    feat: add /my/info API with Swagger note on gateway header injection

--- CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/controller/UserApiController.java
+++ CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/controller/UserApiController.java
@@ -1,6 +1,5 @@
 package com.caring.user_service.presentation.user.controller;
 
-import com.caring.user_service.common.annotation.ManagerCode;
 import com.caring.user_service.presentation.user.service.GetUserProfileUseCase;
 import com.caring.user_service.presentation.user.service.GetUserShelterUuidUseCase;
 import com.caring.user_service.presentation.user.vo.ResponseUser;
@@ -11,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,16 +25,28 @@ public class UserApiController {
     private final GetUserShelterUuidUseCase getUserShelterUuidUseCase;
 
     @Operation(summary = "특정 유저의 계정을 조회합니다.")
-    @GetMapping("/{userUuid}")
-    public ResponseUser getUserProfile(@PathVariable String userUuid,
-            @ManagerCode String managerCode) {
-        return getUserProfileUseCase.execute(userUuid);
+    @GetMapping("/{memberCode}")
+    public ResponseUser getUserProfile(@PathVariable String memberCode) {
+        return getUserProfileUseCase.execute(memberCode);
     }
+
+    @Operation(
+            summary = "내 정보를 조회합니다. (홈 화면 X)",
+            description = """
+                    프론트엔드는 Authorization 헤더(JWT)만 전송하면 됩니다.
+                    Gateway에서 JWT를 파싱하여 자동으로 'member-code' 헤더를 추가하므로,
+                    'member-code' 헤더는 프론트에서 명시적으로 포함시킬 필요가 없습니다.
+                     포스트맨에서 테스트 가능합니다.""")
+    @GetMapping("/my/info")
+    public ResponseUser getMyInfo(@RequestHeader("member-code") String memberCode) {
+        return getUserProfileUseCase.execute(memberCode);
+    }
+
+
 
     @Operation(summary = "특정 유저의 shelterUuid를 반환합니다. 유저가 없으면 404를 반환합니다.")
     @GetMapping("/{userUuid}/shelterUuid")
-    public ResponseUserShelterUuid getUserShelterUuid(@PathVariable String userUuid,
-            @ManagerCode String managerCode) {
+    public ResponseUserShelterUuid getUserShelterUuid(@PathVariable String userUuid) {
         return getUserShelterUuidUseCase.execute(userUuid);
     }
 
