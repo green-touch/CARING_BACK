2cad9115e0ef142ef5bbc3a60264a03c152f1f43
KYS <ehcl1027@gmail.com>
2025-07-10 11:43:26 +0900

    refactor: inject UserMapper via DI and add emergency contact & extended user info

--- CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/mapper/UserMapper.java
+++ CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import com.caring.user_service.domain.user.entity.User;
 import com.caring.user_service.presentation.user.vo.ResponseUser;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 /**
  * vo는 setter를 쓰지 않기 때문에 impl생성이 자동으로 builder가 될 수 있도록 아래와 같이 설정해줘야한다.
