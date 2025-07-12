572c8c30d33305dd4b0eda298531b0ed7d472636
KYS <ehcl1027@gmail.com>
2025-07-12 15:01:44 +0900

    refactor: add final to UserVO fields to ensure immutability

--- CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/vo/RequestEmergencyContact.java
+++ CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/vo/RequestEmergencyContact.java
@@ -3,17 +3,15 @@ package com.caring.user_service.presentation.user.vo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class RequestEmergencyContact {
 
-    private String memberCode; // 유저 memberCode
-    private String contactName;
-    private String contactRelationship;
-    private String contactPhoneNumber;
+    private final String memberCode; // 유저 memberCode
+    private final String contactName;
+    private final String contactRelationship;
+    private final String contactPhoneNumber;
 }
 
