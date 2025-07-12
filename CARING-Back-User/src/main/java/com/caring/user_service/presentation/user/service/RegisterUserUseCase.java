272c9bf1ac64cf0e2de82ed425e2d4e720056252
KYS <ehcl1027@gmail.com>
2025-07-12 15:00:55 +0900

    feat: add UserDTO to simplify method parameters

--- CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/service/RegisterUserUseCase.java
+++ CARING-Back-User/src/main/java/com/caring/user_service/presentation/user/service/RegisterUserUseCase.java
@@ -2,6 +2,7 @@ package com.caring.user_service.presentation.user.service;
 
 import com.caring.user_service.common.annotation.UseCase;
 import com.caring.user_service.domain.user.business.domainservice.UserDomainService;
+import com.caring.user_service.presentation.user.mapper.UserMapper;
 import com.caring.user_service.presentation.user.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RegisterUserUseCase {
     private final UserDomainService userDomainService;
+    private final UserMapper userMapper;
 
     public String execute(RequestUser requestUser) {
-        return userDomainService.registerUser(requestUser.getName(), requestUser.getPassword(),
-                requestUser.getBirthdate(), requestUser.getPhoneNumber(),
-                requestUser.getRoadAddress(),requestUser.getDetailAddress()).getMemberCode();
+        return userDomainService.registerUser(userMapper.toDTO(requestUser)).getMemberCode();
     }
 }
