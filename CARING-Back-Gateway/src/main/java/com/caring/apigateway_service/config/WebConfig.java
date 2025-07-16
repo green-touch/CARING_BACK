99055566811f4032fd42fe7e05c0c1de9621016c
hann <wjdgks1233@gmail.com>
2025-07-16 22:10:39 +0900

    [CARE-109] hotfix : log

--- CARING-Back-Gateway/src/main/java/com/caring/apigateway_service/config/WebConfig.java
+++ CARING-Back-Gateway/src/main/java/com/caring/apigateway_service/config/WebConfig.java
@@ -4,14 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,22 +24,8 @@ public class WebConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(Customizer.withDefaults())
                 .build();
     }
 
-    @Bean
-    public CorsWebFilter corsFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList(
-                env.getProperty("openapi.server.url")
-        )); // 허용할 Origin 지정
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); // 허용할 HTTP 메서드
-        corsConfig.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
-        corsConfig.setAllowCredentials(true); // 자격 증명(쿠키, HTTP 인증 등) 허용
-        corsConfig.setMaxAge(3600L); // Pre-flight 요청 캐싱 시간
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig); // 모든 경로에 대해 CORS 설정 적용
-        return new CorsWebFilter((CorsConfigurationSource) source);
-    }
 }
