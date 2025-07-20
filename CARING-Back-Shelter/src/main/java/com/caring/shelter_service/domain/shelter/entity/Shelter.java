a8aa0d4cd7d6e1b95deda03ae041791af3d864b7
hann <wjdgks1233@gmail.com>
2025-07-20 20:16:54 +0900

    [CARE-113] feat : add shelter entity

--- CARING-Back-Shelter/src/main/java/com/caring/shelter_service/domain/shelter/entity/Shelter.java
+++ CARING-Back-Shelter/src/main/java/com/caring/shelter_service/domain/shelter/entity/Shelter.java
@@ -1,4 +1,24 @@
 package com.caring.shelter_service.domain.shelter.entity;
 
-public class Shelter {
+import com.caring.shelter_service.domain.auditing.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+public class Shelter extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shelter_id")
+    private Long id;
+
+    private String name;
 }
